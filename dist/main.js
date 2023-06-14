09ed60bc6fdf5b736dfe267208ead3f66d50c361
shinnshi <shin.nshi3@gmail.com>
2023-06-14 17:33:31 +0900

    ADD: Package added and modified to work

---
+++ dist/main.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+const num = +process.argv[2];
+console.log(2123);
+function fizzbuzz(num) {
+    if (num % 15 == 0) {
+        return "FizzBuzz";
+    }
+    else if (num % 3 == 0) {
+        return "Fizz";
+    }
+    else if (num % 5 == 0) {
+        return "Buzz";
+    }
+    return num.toString();
+}
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7QUFBYTtBQUNiO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9hdGNvZGVyLWRlYnVnLXR5cGVzY3JpcHQvLi9zcmMvaW5kZXgudHMiXSwic291cmNlc0NvbnRlbnQiOlsiXCJ1c2Ugc3RyaWN0XCI7XG5jb25zdCBudW0gPSArcHJvY2Vzcy5hcmd2WzJdO1xuY29uc29sZS5sb2coMjEyMyk7XG5mdW5jdGlvbiBmaXp6YnV6eihudW0pIHtcbiAgICBpZiAobnVtICUgMTUgPT0gMCkge1xuICAgICAgICByZXR1cm4gXCJGaXp6QnV6elwiO1xuICAgIH1cbiAgICBlbHNlIGlmIChudW0gJSAzID09IDApIHtcbiAgICAgICAgcmV0dXJuIFwiRml6elwiO1xuICAgIH1cbiAgICBlbHNlIGlmIChudW0gJSA1ID09IDApIHtcbiAgICAgICAgcmV0dXJuIFwiQnV6elwiO1xuICAgIH1cbiAgICByZXR1cm4gbnVtLnRvU3RyaW5nKCk7XG59XG4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
