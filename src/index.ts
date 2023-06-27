0b61eae018e7378ccbf5cfb736e886102831f67d
shinnshi <shin.nshi3@gmail.com>
2023-06-27 19:05:53 +0900

    fix: create branch

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 
 
 function Main(input: any) {
-  input = input.split("\n"); //[a, b c, s]
-  console.log(input)
+  input = input.split("\n");
+  const calculation = parseInt(input[0]) * parseInt(input[1])
+
+  if (calculation % 2 !== 0) {
+    console.log('Odd')
+
+  } else {
+    console.log('Even')
+  }
 }
 
-Main(`a
-b c
-s
-`);
+Main(`0
+1901`);
 
 // Main(require("fs").readFileSync("/dev/stdin", "utf8")); 
